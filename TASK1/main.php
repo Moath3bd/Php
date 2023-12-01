210a5f600fd44c98c239be4ac21966f9d5cbb317
moath3bd <moath3bd@gmail.com>
2023-12-01 20:44:46 +0300

    two three

--- TASK1/main.php
+++ TASK1/main.php
@@ -8,3 +8,13 @@ echo "<table border=1 cellspacing=0 cellpading=0>
 <tr> <td><font color=blue>Salary of Mr. C is</td> <td>$c$</font></td></tr>
 </table>";
 ?>
+<?php
+$current_file_name = basename($_SERVER['PHP_SELF']);
+$file_last_modified = filemtime($current_file_name); 
+echo "Last modified " . date("l, dS F, Y, h:ia", $file_last_modified)."\n";
+?>
+<?php
+$file = basename($_SERVER['PHP_SELF']); 
+$no_of_lines = count(file($file)); 
+echo "There are $no_of_lines lines in $file"."\n";
+?>
