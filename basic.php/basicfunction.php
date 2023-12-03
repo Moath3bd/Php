529f25e79b5ece1f2ca90481b0e9726f88940ecf
moath3bd <moath3bd@gmail.com>
2023-12-03 15:23:59 +0300

    task 3

--- basic.php/basicfunction.php
+++ basic.php/basicfunction.php
@@ -31,4 +31,17 @@ function Reverse($str)
   
 $str = "moath"; 
 echo Reverse($str)
+// Task 3
+function is_str_lowercase($str1)
+   {
+    for ($sc = 0; $sc < strlen($str1); $sc++) {
+	      if (ord($str1[$sc]) >= ord('A') &&
+          ord($str1[$sc]) <= ord('Z')) {
+      return false;
+         }
+         }
+      return true;
+       }
+var_dump(is_str_lowercase('abc def ghi'));
+var_dump(is_str_lowercase('abc dEf ghi'));
  ?>
