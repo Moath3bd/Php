1c99fc20a54d4d84d70a160b91d29d68e29f32d6
moath3bd <moath3bd@gmail.com>
2023-12-07 11:38:29 +0300

    task5.1

---
+++ Task5/config.php
@@ -0,0 +1,15 @@
+<?php
+$host = "your_host";
+$dbname = "your_database";
+$username = "your_username";
+$password = "your_password";
+
+try
+ {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e)
+ {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
